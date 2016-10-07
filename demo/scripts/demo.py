52f2e3eeb1e2f3255aec9436a2955e2d133f072c
Kevin French <fnivek@gmail.com>
2016-10-06 20:00:45 -0400

    ROS: Publish video information in wrong format

--- demo/scripts/demo.py
+++ demo/scripts/demo.py
@@ -3,6 +3,8 @@
 
 import rospy
 import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
 
 def main():
@@ -15,9 +17,16 @@ def main():
     cap.set(3, 640)
     cap.set(4, 480)
 
+    # Ros publisher
+    vid_pub = rospy.Publisher('demo_vid', Image, queue_size=1)
+
+    cv_bridge = CvBridge()
+
     while not rospy.is_shutdown():
         _, frame = cap.read()
         cv2.imshow('demo', frame)
+        image_message = cv_bridge.cv2_to_imgmsg(frame, encoding="passthrough")
+        vid_pub.publish(image_message)
 
         key = 0xFF & cv2.waitKey(10)
         if key == 0x1B:
